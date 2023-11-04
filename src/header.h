743e83fa0299dadba24eb087e1f9e3bc87738800
psyhs10 <psyhs10@exmail.nottingham.ac.uk>
2023-11-04 12:14:03 +0000

    hello

---
+++ src/header.h
@@ -0,0 +1,17 @@
+
+#define ui32 __uint32_t
+
+
+typedef struct vector2 {
+    ui32 x;
+    ui32 y;
+} vector2;
+
+struct wall {
+    ui32 x1;
+    ui32 y1;
+    ui32 x2;
+    ui32 y2;
+    ui32 sector;
+
+} wall;
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main(){
+
+
+
+
+
+    return 0;
+}
